a5ca572ea46de1a2104d1c3006b68fe0dd1d0656
formaction <formaction@163.com>
2016-10-20 11:17:33 +0800

    bug

--- gulpfile.js
+++ gulpfile.js
@@ -110,7 +110,17 @@ gulp.task('default', ['images', 'less', 'webpack', 'media'], function() {
 });
 
 //自动刷新
-gulp.task('sync', ['images', 'less', 'webpack', 'media','browsersync']);
+gulp.task('sync', ['images', 'less', 'webpack', 'media','browsersync'], function() {
+
+    gulp.watch(imageSrc, ['images']);
+    
+    gulp.watch(lessDict, ['less']);
+    
+    gulp.watch(jsSrc, ['webpack']);
+
+    gulp.watch(media, ['media']);
+
+});
 
 //压缩
 gulp.task('zip', ['images', 'lessmin', 'webpack:build', 'media']);
