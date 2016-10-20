a0680528c8319bbca12748a11716d6863e1ea780
formaction <formaction@163.com>
2016-10-20 10:29:26 +0800

    集成browsersync自动刷新

---
+++ README.md
@@ -0,0 +1,15 @@
+# TD
+
+0. 安装nodejs环境
+1. 运行npm install 安装对应的node依赖包
+2. 编译js和css运行：gulp
+3. 实时编译: gulp
+5. 浏览器自动刷新 + 实时编译：gulp sync
+4. 压缩编译: gulp zip
+
+##图片自动转base64，改"url"为"datauri"
+    background-image: datauri(..);
+
+## License
+
+[MIT](https://opensource.org/licenses/MIT)
---
+++ config.js
@@ -0,0 +1,44 @@
+
+module.exports = {
+    webpack: {
+        entry: {
+            main: "./src/js/index.js",
+        },
+        output: {
+            path: '.',
+            filename: "./dist/js/[name].js"
+        },
+        module: {
+            loaders: [
+                {
+                    test: /js[\/|\\]lib[\/||\\][\w|\.|_|-]+js$/,
+                    loader: 'url-loader?importLoaders=1&limit=1000&name=/dist/js/lib/[name].[ext]'
+                },
+                {
+                    test: /\.hbs/,
+                    loader: "handlebars-loader"
+                }
+            ]
+        },
+        resolve: {
+            alias: {
+                'zepto': './lib/zepto.min.js'
+            }
+        },
+        plugins: [
+            
+        ],
+        externals: {
+            '$':'window.$',
+            'global' : 'window.global'
+        }
+    },
+    browsersync: {
+        open: "external",
+        server: {
+            baseDir: "./"
+        },
+        port: 80,
+        files: ["dist/**/*.*","./*.html"]
+    }
+};
